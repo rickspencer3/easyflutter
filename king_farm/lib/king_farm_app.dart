0b175b962d1021562dc540243f7f65f03f12126f
Rick Spencer <richard.linger.spencer.3@gmail.com>
2018-09-15 21:35:52 -0400

    added custom colors

--- king_farm/lib/king_farm_app.dart
+++ king_farm/lib/king_farm_app.dart
@@ -5,7 +5,7 @@ import './app_page.dart';
 class KingFarmApp {
   static String title = "King Farm";
 
-  static ThemeData themeData = ThemeData(primaryColor: Colors.indigo);
+  static ThemeData themeData = ThemeData(primaryColor: Color.fromARGB(255, 87, 99, 46));
 
   static List<AppPage> pages = [
     new AppPage(
